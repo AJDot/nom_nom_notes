1d46e3446d83bc701f758486fdf1ae9df5675caa
Alex Henegar <alex.henegar19@gmail.com>
2021-01-27 19:07:12 -0500

    refactor: move sign out action to store

--- src/store/modules/sessions/actions.ts
+++ src/store/modules/sessions/actions.ts
@@ -1,11 +1,14 @@
 import { Action, ActionTree } from 'vuex'
 import { RootState, SessionsState } from '~/store/interfaces'
 import RoutePath from '~/router/path'
-import { plainAxiosInstance } from '~/backend/axios'
+import { plainAxiosInstance, securedAxiosInstance } from '~/backend/axios'
 import { AxiosResponse } from 'axios'
+import { SessionMutationTypes } from '~/store/modules/sessions/mutations'
+import { HttpStatusCode } from '~/utils/httpUtils'
 
 export enum SessionActionTypes {
   CREATE = 'CREATE',
+  DESTROY = 'DESTROY',
 }
 
 type SessionActions = {
@@ -16,6 +19,13 @@ const actions: ActionTree<SessionsState, RootState> & SessionActions = {
   async [SessionActionTypes.CREATE](_store, payload: {email: string, password: string}): Promise<AxiosResponse> {
     return plainAxiosInstance
       .post(RoutePath.signin(), payload)
+  },
+  async [SessionActionTypes.DESTROY]({ commit }): Promise<AxiosResponse> {
+    const response = await securedAxiosInstance.delete(RoutePath.signin())
+    if (response.status === HttpStatusCode.Ok) {
+      commit(SessionMutationTypes.SIGN_OUT)
+    }
+    return response
   },
 }
 
