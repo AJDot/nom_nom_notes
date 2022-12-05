cc7f4d38db02078ae539a5125742ee18532498ad
Alex Henegar <alex.henegar19@gmail.com>
2022-12-04 20:33:44 -0500

    search dropdowns add blue highlight, tab support, arrow up/down support, enter support [sc-81]

--- src/components/search.vue
+++ src/components/search.vue
@@ -2,11 +2,11 @@
   <div>
     <dropdown :state="dropdownState" @close="hideResults">
       <template #control>
-        <a-input ref="search" v-model="q" :id="id" type="search" @keyup="search" @keydown.enter.prevent placeholder="Search..." class="mt-1" />
+        <a-input ref="search" v-model="q" :id="id" type="search" @keyup="search" @keydown.enter.prevent="select(currentResult)" placeholder="Search..." class="mt-1" @keydown.down="down" @keydown.up="up" @blur="(currentResult = null)" />
       </template>
       <ul>
         <template v-if="hasResults">
-          <dropdown-item v-for="item in results" :key="item.value" @click.capture="select(item)">
+          <dropdown-item v-for="item in results" @click="select(item)" :class="{ 'select-blue': item === currentResult }">
             <dropdown-item-button>
               {{ item.label }}
             </dropdown-item-button>
@@ -21,13 +21,14 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, nextTick } from 'vue'
+import { defineComponent } from 'vue'
 import { SearchResult, USearcher } from 'Interfaces/searchInterfaces'
 import AInput from '~/components/structure/a-input.vue'
 
 interface Data {
   q: string
   results: Array<SearchResult<never>>
+  currentResult: SearchResult<never> | null
   dropdownState: boolean
 }
 
@@ -51,6 +52,7 @@ export default defineComponent({
       q: '',
       results: [],
       dropdownState: false,
+      currentResult: null,
     }
   },
   computed: {
@@ -60,22 +62,52 @@ export default defineComponent({
   },
   methods: {
     async search(evt: KeyboardEvent): Promise<void> {
+      if (['ArrowUp', 'ArrowDown', 'Enter'].includes(evt.key)) return
+
       if (this.q) {
         await this.searcher.search(this.q)
         this.results = this.searcher.results
         this.dropdownState = true
+        this.makeCurrent(0)
       } else {
         this.hideResults()
+        this.makeCurrent(null)
       }
     },
     async hideResults(): Promise<void> {
       this.dropdownState = false
     },
-    async select(item: SearchResult<never>): Promise<void> {
+    async select(item: SearchResult<never> | null): Promise<void> {
+      if (!item) return
+
       this.q = ''
       this.hideResults()
       this.$emit('select', { data: item });
       (<InstanceType<typeof AInput>>this.$refs.search).$el.focus()
+    },
+    down(evt: KeyboardEvent) {
+      const length = this.results.length
+      let index = length
+      if (this.currentResult) {
+        index += this.results.indexOf(this.currentResult) + 1
+      }
+      this.makeCurrent(index)
+    },
+    up(evt: KeyboardEvent) {
+      const length = this.results.length
+      let index = length
+      if (this.currentResult) {
+        index += this.results.indexOf(this.currentResult) - 1
+      }
+      this.makeCurrent(index)
+    },
+    makeCurrent(index: number | null) {
+      if (index === null) {
+        this.currentResult = null
+      } else {
+        const length = this.results.length
+        this.currentResult = this.results[index % length]
+      }
     },
   },
 })
