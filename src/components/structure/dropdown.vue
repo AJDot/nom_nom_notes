c873db5f45bb814c3b5b0c2ebb754faf05ac8d90
Alex Henegar <alex.henegar19@gmail.com>
2022-12-23 15:33:29 -0500

    fix dropdown going outside viewport

--- src/components/structure/dropdown.vue
+++ src/components/structure/dropdown.vue
@@ -1,6 +1,6 @@
 <template>
   <slot name="control" />
-  <div class="absolute left-0 right-0" :style="styles">
+  <div class="absolute" :class="positionType === 'relative' ? 'left-0 right-0' : ''" :style="style" ref="dropdown">
     <div v-if="state" class="z-10 border border-gray-400 mt-1 max-h-56 min-w-[14em] overflow-auto rounded-md bg-white text-base shadow-lg focus:outline-none" :class="{ 'right-0': right }" tabindex="-1" role="listbox">
       <slot />
     </div>
@@ -8,11 +8,12 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, StyleValue } from 'vue'
+import { CSSProperties, defineComponent, nextTick } from 'vue'
 import SelectionUtils from '~/utils/selectionUtils'
 
 interface Data {
   position: Omit<DOMRectReadOnly, 'toJSON'> | null
+  style: CSSProperties
 }
 
 export default defineComponent({
@@ -37,24 +38,34 @@ export default defineComponent({
   },
   data(): Data {
     return {
-      position: null
-    }
-  },
-  computed: {
-    styles(): StyleValue {
-      const style: StyleValue = {}
-      if (this.position) {
-        const padding = { x: 0, y: 10 }
-        style.top = `${this.position.bottom + padding.y}px`
-        style.left = `${this.position.x + padding.x}px`
-      }
-      return style
+      position: null,
+      style: {},
     }
   },
   methods: {
+    updateStyles() {
+      if (this.position) {
+        const padding = { x: 0, y: 10 }
+        this.style.top = `${this.position.bottom + padding.y}px`
+        this.style.left = `${this.position.x + padding.x}px`
+      }
+    },
     async close(e: MouseEvent) {
       if (!this.$el.contains(e.target)) this.$emit('close')
-    }, 
+    },
+    handleDropdownPosition() {
+      const screenPadding = 16
+
+      const dropdown = this.$refs.dropdown as HTMLElement
+      const dropdownRect = dropdown.getBoundingClientRect()
+      const dropdownRightX = dropdownRect.x + dropdownRect.width
+
+      if (dropdownRect.x < 0) {
+        this.style.transform = `translateX(${dropdownRect.x + screenPadding}px)`
+      } else if (dropdownRightX > window.innerWidth) {
+        this.style.transform = `translateX(${(window.innerWidth - dropdownRightX) - screenPadding}px)`
+      }
+    }
   },
   mounted() {
     document.addEventListener('click', this.close)
@@ -63,9 +74,15 @@ export default defineComponent({
     document.removeEventListener('click', this.close)
   },
   watch: {
-    state(newVal, oldVal) {
+    async state(newVal, oldVal) {
       if (newVal && this.positionType === 'cursor') {
         this.position = SelectionUtils.getCaretRect()
+        this.updateStyles()
+        await nextTick()
+        this.handleDropdownPosition()
+      } else {
+        this.position = null
+        this.style = {}
       }
     }
   }
