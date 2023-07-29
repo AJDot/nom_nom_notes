3a81273eefb5efc61ef9021d9b1f028c674a7fb7
Alex Henegar <alex.henegar19@gmail.com>
2023-07-29 13:06:42 -0400

    test number block [sc-262]

--- cypress/e2e/dynamicRecipes/scale-spec.cy.ts
+++ cypress/e2e/dynamicRecipes/scale-spec.cy.ts
@@ -2,7 +2,7 @@ import { Block } from '../../../src/interfaces/blockInterfacesGeneral'
 import Guid from '../../../src/utils/guid'
 
 describe('Dynamic Recipe Scale', () => {
-  beforeEach(function () {
+  beforeEach(function() {
     cy.createFry().as('fry')
     cy.forceSignIn()
   })
@@ -54,24 +54,50 @@ describe('Dynamic Recipe Scale', () => {
     cy.contains('1/2lemon').should('exist')
   })
 
-  it('only works in "show" mode', function() {
+  it('multiplies number block text by amount chosen', function() {
+    const blocks: Block[] = [
+      { id: Guid.create(), type: 'number', content: { text: '1 1/2' } },
+      { id: Guid.create(), type: 'number', content: { text: '4' } },
+    ]
     cy.apiRequest('POST', '/testing/api/v1/dynamic_recipes', {
-      dynamic_recipe: {
-        name: 'Pasta',
-        ownerId: this.fry.attributes.clientId,
-        blocks: [
-          { id: Guid.create(), type: 'ingredient', content: { quantity: '1 1/2 cup', name: 'banana', text: '' } },
-        ] as Block[],
-      },
+      dynamic_recipe: { name: 'Pasta', ownerId: this.fry.attributes.clientId, blocks },
+    })
+    cy.visit('/dynamic_recipes')
+    cy.contains('Pasta').click()
+    cy.contains('1 1/2').should('exist')
+    cy.contains('4').should('exist')
+    cy.contains('button', 'Scale').click()
+
+    cy.contains(/^2$/).click() // regex so 1/3 isn't found by mistake
+    cy.contains('3').should('exist')
+    cy.contains('8').should('exist')
+
+    cy.contains('button', '(2) Scale').click()
+    cy.getDropdownItem('1/2').click()
+    cy.getTest('dropdown-item').should('not.exist')
+    cy.contains('3/4').should('exist')
+    cy.contains('2').should('exist')
+  })
+
+  it('only works in "show" mode', function() {
+    const blocks: Block[] = [
+      { id: Guid.create(), type: 'ingredient', content: { quantity: '1 1/2 cup', name: 'banana', text: '' } },
+      { id: Guid.create(), type: 'number', content: { text: '4' } },
+    ]
+    cy.apiRequest('POST', '/testing/api/v1/dynamic_recipes', {
+      dynamic_recipe: { name: 'Pasta', ownerId: this.fry.attributes.clientId, blocks },
     })
     cy.visit('/dynamic_recipes')
     cy.contains('Pasta').click()
     cy.contains('button', 'Scale').click()
     cy.contains('1 1/2 cup').should('exist')
+    cy.contains('4').should('exist')
     cy.getDropdownItem('1 1/2').click()
     cy.getTest('dropdown-item').should('not.exist')
     cy.contains('1 1/2 cup').should('not.exist')
     cy.contains('2 1/4 cup').should('exist')
+    cy.contains(/^4$/).should('not.exist')
+    cy.contains(/^6$/).should('exist')
 
     // in edit mode the original user data is present and ready for editing
     cy.contains('Edit').click()
@@ -79,15 +105,21 @@ describe('Dynamic Recipe Scale', () => {
     cy.contains('(1 1/2) Scale').should('not.exist')
     cy.contains('1 1/2 cup').should('exist')
     cy.contains('2 1/4 cup').should('not.exist')
+    cy.contains(/^4$/).should('exist')
+    cy.contains(/^6$/).should('not.exist')
 
     // change quantity
     cy.getContentEditable('1 1/2 cup').clear()
     cy.getContentEditableEmpty('Quantity').type('2 tablespoons')
+    cy.getContentEditable(/^4$/).clear()
+    cy.getContentEditableEmpty('12.34').type('10')
 
     // scale is remembered so 2 tbsp is scaled by 1 1/2
     cy.contains('Back').click()
     cy.contains('3 tablespoons').should('exist')
     cy.contains('2 tablespoons').should('not.exist')
+    cy.contains('15').should('exist')
+    cy.contains('10').should('not.exist')
     cy.contains('button', '(1 1/2) Scale').should('exist')
   })
 })
